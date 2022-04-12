64ab5aecc63b92628f55123d7e511b0c4366c0eb
izubethel <michaelokpalaoguchi@gmail.com>
2022-04-12 15:21:38 -0400

    Create even.js

---
+++ even.js
@@ -0,0 +1,25 @@
+// write a function that return the sum of even numbers
+
+//let sum = 0;
+//for(i=1;i<=100;i++){
+//if(i%2==0)
+//{
+//sum=sum+i;
+//}
+//}
+//console.log(sum);
+
+
+
+var Numbers = 100;
+var evenSum = 0;
+
+    for(var i=1; i < Numbers; i++){
+        if (i%2==0) {
+            console.log(i);
+            evenSum=sum+i;
+            
+        }
+    }
+    
+console.log(evenSum);
