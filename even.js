c4d1debe22039bb3c9b34fe173e671043f5cc86f
izubethel <michaelokpalaoguchi@gmail.com>
2022-04-13 14:07:05 -0400

    Update even.js

--- even.js
+++ even.js
@@ -11,15 +11,33 @@
 
 
 
-var Numbers = 100;
-var evenSum = 0;
-
-    for(var i=1; i < Numbers; i++){
+function evenSum(num){
+    var sum = 0;
+    for(var i=1; i <= num; i++){
         if (i%2==0) {
-            console.log(i);
-            evenSum=sum+i;
-            
+            sum=sum+i;     
         }
     }
-    
+    return sum;
+}  
+var getSum = evenSum(100);  
 console.log(evenSum);
+
+
+//Assessment 2: Write a condition inside the sumofeven function that ensure start parameter is less than end parameter
+
+function sumOfEven(start,end){
+    if(typeof start !== 'number' || typeof end !== 'number' || start >= end){
+      return 'Invalid number supplied';
+    }
+  
+    var sum = 0;
+    for(var i=start; i<=end; i++){
+      if(i%2===0){
+        sum = sum + i;
+      }
+    }
+    return sum;
+  }
+  var getSum = sumOfEven();
+  console.log(getSum);
